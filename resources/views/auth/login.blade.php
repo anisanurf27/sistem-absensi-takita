843bc12c34b6ab78cb07b6869a0428d72b0dddb9
Anisa <anisanurfadhilah27@gmail.com>
2024-04-22 09:56:08 +0700

    resolve: asset-template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,51 +1,52 @@
-{{-- <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+{{-- OLD Layout (tidak dipakai)--}}
+	{{-- <x-guest-layout>
+		<!-- Session Status -->
+		<x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+		<form method="POST" action="{{ route('login') }}">
+			@csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+			<!-- Email Address -->
+			<div>
+				<x-input-label for="email" :value="__('Email')" />
+				<x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+				<x-input-error :messages="$errors->get('email')" class="mt-2" />
+			</div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+			<!-- Password -->
+			<div class="mt-4">
+				<x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+				<x-text-input id="password" class="block mt-1 w-full"
+								type="password"
+								name="password"
+								required autocomplete="current-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+				<x-input-error :messages="$errors->get('password')" class="mt-2" />
+			</div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+			<!-- Remember Me -->
+			<div class="block mt-4">
+				<label for="remember_me" class="inline-flex items-center">
+					<input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+					<span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+				</label>
+			</div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+			<div class="flex items-center justify-end mt-4">
+				@if (Route::has('password.request'))
+					<a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+						{{ __('Forgot your password?') }}
+					</a>
+				@endif
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout> --}}
-
+				<x-primary-button class="ms-3">
+					{{ __('Log in') }}
+				</x-primary-button>
+			</div>
+		</form>
+	</x-guest-layout> --}}
+{{-- OLD Layout (tidak dipakai)--}}
 
 {{-- new layout --}}
 <!DOCTYPE html>
@@ -56,7 +57,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Presensix - Login</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon"  href="{{url('assetImg/ticket8.png')}}">
+    <link rel="icon"  href="{{url('assetImg/logo-judul.png')}}">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	
